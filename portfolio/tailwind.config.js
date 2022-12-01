8a6be2cd6680fbad1bc02362bd4918e9b84cad66
Michael Chen <73071479+Blanku-cloud@users.noreply.github.com>
2022-12-01 11:21:01 -0500

    Made vue with tailwind work

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,14 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './components/**/*.{js,vue,ts}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
